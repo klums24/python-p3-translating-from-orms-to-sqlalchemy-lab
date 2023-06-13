fe45653822b4953a973538cf4e168e48cc466f23
Kevin Lumauig <klums24@gmail.com>
2023-06-13 10:06:55 -0700

    translating lab most complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,36 @@
 from models import Dog
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 def create_table(base):
-    pass
+    engine = create_engine("sqlite:///:memory:")
+    base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
+
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
+    
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
+
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
+
+
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
